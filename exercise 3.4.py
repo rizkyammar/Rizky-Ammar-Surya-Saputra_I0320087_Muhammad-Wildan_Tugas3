1c4425837365bb65ce0d2362a0aec6f472e56e53
rizkyammar <80144906+rizkyammar@users.noreply.github.com>
2021-03-18 13:47:50 +0700

    Add files via upload

---
+++ exercise 3.4.py
@@ -0,0 +1,5 @@
+#Contoh cara membuat Dictionary pada Phyton
+
+dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+print("dict['Name']: ", dict['Name'])
+print("dict['Age']: ", dict['Age'])
---
+++ exercise 3.5.py
@@ -0,0 +1,5 @@
+#Contoh cara membuat Dictionary
+
+dict={'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+print("dict['Name']: ", dict['Name'])
+print("dict['Age']: ", dict['Age'])
