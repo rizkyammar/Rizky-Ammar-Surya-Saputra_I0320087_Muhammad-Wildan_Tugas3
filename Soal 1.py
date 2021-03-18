406d9830cc335c595eec11d5ea8ab579ba08bce4
rizkyammar <80144906+rizkyammar@users.noreply.github.com>
2021-03-18 14:46:33 +0700

    Add files via upload

---
+++ Soal 1.py
@@ -0,0 +1,31 @@
+#List 10 Nama Teman
+list = ['Tito', 'Sony', 'Yeario', 'Rizky', 'Alvin', 'Ryan', 'Rahmat', 'Vincentius', 'William', 'Rizal']
+
+print(list)
+print("Isi list indeks 4,6,dan  7: ", list[4], list[6], list[7])
+
+#Ganti nama teman di list 3,5,9
+print("List 3,5,9 sebelum diganti: ", list[3], list[5], list[9])
+
+list[3] = 'Anindra'
+list[5] = 'Anderson'
+list[9] = 'Adam'
+
+print("List 3,5,9 setelah diganti: ", list[3], list[5], list[9])
+
+#Menambahkan dua nama teman
+print("List sebelum ditambah: ", list)
+list.append('Surya')
+list.append('Saputra')
+print("List setelah ditambah: ", list)
+
+#menampilkan panjang list
+len(list)
+print("Menampilkan panjang list: ", len(list))
+
+
+
+
+
+
+
---
+++ Soal 2.py
@@ -0,0 +1,17 @@
+#Membuat Dictionary
+
+print("Membuat Dictionary")
+dict = {'Nama': 'Rizky Ammar Surya Saputra', 'Hobby': 'Olahraga, Nonton Film', 'Sosial Media': 'Instagram : @ammaar418, Line : @rizkyammar123', 'Lagu Kesukaan': 'Pamungkas : Tobe bone, Fiersa Besari : Celengan Rindu', 'Makanan Favorit': 'Nasi Goreng, Telor Dadar'}
+print(dict)
+
+#Mengubah Salah Satu Hobby dan Sosisal Media
+print("Mengubah Salah Satu Isi Hobby dan Sosial Media")
+dict['Hobby'] = 'Olahraga, Renang'
+dict['Sosial Meedia'] = 'Instagram : @ammaar418, Gmail : rizkyammar123@gmail.com'
+print(dict)
+
+#Menambahkan Satu Hobby
+print("Dictionary Sebelum ditambah: ", dict)
+dict['Hobby'] = 'Olahraga, Nonton Film, Bermain Catur'
+print("Dictionary setelah ditambah")
+print(dict)
